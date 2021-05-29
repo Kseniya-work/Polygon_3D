5dc0359b7cb03ea53608a36ad1af35ae5c86cdde
Kseniya <kseniya.nosulko@sofoil.com>
2021-05-29 19:37:15 +0300

    fix code duplication for coordinates input

--- test/test_call.cpp
+++ test/test_call.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <exception>
+#include <array>
+
 
 #include <source/polygon_3D/polygon_3D.h>
 
@@ -23,44 +25,31 @@ int main() {
         throw std::runtime_error(oss.str());  
     }
 
-    std::string ix_str = "", iy_str = "", iz_str = "";
+    std::array<double, 3> icoordinates = {0.0, 0.0, 0.0}; // x, y, z
+    std::string coordinates_name = "xyz";
     std::cout << "Please enter a point to calculate projection." << std::endl;
-    std::cout << "x: ";
-    std::cin >> ix_str;
-    std::cout << "y: ";
-    std::cin >> iy_str;
-    std::cout << "z: ";
-    std::cin >> iz_str;
-
-    double ix = 0.0, iy = 0.0, iz = 0.0;
-    try {
-        ix = std::stod(ix_str);
+    for(int i = 0; i < coordinates_name.length(); i++){
+        std::cout << coordinates_name[i] << ": ";
+        std::string icoordinate_str = "";
+        std::cin >> icoordinate_str;
+        try {
+            icoordinates[i] = std::stod(icoordinate_str);
+        }
+        catch (const std::exception& e) {
+            throw std::runtime_error(
+                std::string("input value of coordanate ")
+                + coordinates_name[i]
+                + std::string(" is invalid: ")
+                + e.what()
+            );
+        }
     }
-    catch (const std::exception& e) {
-		throw std::runtime_error(
-			std::string("input value of coordanate x is invalid: ") + e.what()
-		);
-	}
-    try {
-        iy = std::stod(iy_str);
-    }
-    catch (const std::exception& e) {
-		throw std::runtime_error(
-			std::string("input value of coordanate y is invalid: ") + e.what()
-		);
-	}
-    try {
-        iz = std::stod(iz_str);
-    }
-    catch (const std::exception& e) {
-		throw std::runtime_error(
-			std::string("input value of coordanate z is invalid: ") + e.what()
-		);
-	}
 
     polygon::Polygon_3D poly;
     poly.parse(is);
-    poly.calc_projections(ix, iy, iz);
+    poly.calc_projections(icoordinates[0], 
+                          icoordinates[1], 
+                          icoordinates[2]);
     poly.show_results();
 
     return 0;
